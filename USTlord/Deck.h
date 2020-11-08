e4bb2c44a0fe0e87be80f1ebc341f826793cb74c
williamlus <67042315+williamlus@users.noreply.github.com>
2020-11-08 16:36:54 +0800

    Update Deck.h

--- USTlord/Deck.h
+++ USTlord/Deck.h
@@ -11,7 +11,7 @@ class Deck {
       int number_of_cards;//Number of cards in deck
     public:
       Deck();//default constructor
-      Deck(Vector<Card*> card);//Conersion constructor, i means the number of cards in this deck
+      Deck(vector<Card*> card);//Conersion constructor, i means the number of cards in this deck
       ~Deck();
       Card* operator[](int i);
       //void shuffle();//shuffle cards in deck//we will do shuffle in board
