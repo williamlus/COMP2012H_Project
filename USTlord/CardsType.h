7dd306281e4f08b96dd3a5606887bc91b4b777e7
williamlus <67042315+williamlus@users.noreply.github.com>
2020-11-08 19:38:20 +0800

    Update CardsType.h

--- USTlord/CardsType.h
+++ USTlord/CardsType.h
@@ -4,9 +4,14 @@
 using std::string;
 
 class CardsType {
+public:
+    enum class Type {
+        ROCKET, BOMB, SINGLE, PAIR, TRIO, TRIO_WITH_ONE,
+        SINGLE_CONTINUOUS, PAIR_CONTINUOUS, TRIO_CONTINUOUS,
+        PLANE, FOUR_WITH_TWO
+    };
 private:
-    int typeID;
-    string type;
+    Type type;
     int num_cards;//number of cards in this group
     int num_same_value;//number of cards with same value
     bool continuous;//whether the cards are continuous
@@ -14,19 +19,17 @@ public:
     CardsType();
     ~CardsType();
     bool is_equal(const CardsType& ct) const;//compare whether two cardstype object are equal(can also be defined as bool operator==(const CardsTpe& ct);)
-    
+    string to_string() const;//return the type by string
+
     //accessors
-    int get_typeID() const;
-    string get_type() const;
+    Type get_type() const;
     int get_num_cards() const;
     int get_num_same_value() const;
     bool is_continuous() const;
 
     //mutators
-    void set_typeID(int typeID);
-    void set_type(string type);
+    void set_type(Type type);
     void set_num_cards(int num_cards);
     void set_num_same_value(int num_same_value);
     void set_continuous(bool continuous);
-
 };
