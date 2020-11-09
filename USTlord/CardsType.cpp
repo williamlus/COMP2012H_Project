ba56373a82621a2960a465922883838525ce1c64
williamlus <67042315+williamlus@users.noreply.github.com>
2020-11-09 22:29:42 +0800

    Update CardsType.cpp

--- USTlord/CardsType.cpp
+++ USTlord/CardsType.cpp
@@ -3,59 +3,42 @@
 #include <string>
 using std::string;
 
+//Constructor
 CardsType::CardsType():type(Type::EMPTY),num_cards(0),num_same_value(0),continuous(false){}
+CardsType::CardsType(Type type,int num_cards,int num_same_value,bool continuous):
+	type(type),num_cards(num_cards),num_same_value(num_same_value),continuous(continuous){}
+
+//Destructor
 CardsType::~CardsType(){}
-bool CardsType::is_equal(const CardsType& ct) const{
-	if (this->type == ct.type && this->num_cards == ct.num_cards && this->num_same_value == ct.num_same_value && this->continuous == ct.continuous) {
+
+//Binary Operation
+bool CardsType::operator==(const CardsType& ct) const{
+	if (this->type == ct.type && this->num_cards == ct.num_cards 
+	&& this->num_same_value == ct.num_same_value && this->continuous == ct.continuous) {
 		return true;
 	}
 	return false;
 }//compare whether two cardstype object are equal(can also be defined as bool operator==(const CardsTpe& ct);)
+
+//Accessors
 string CardsType::to_string() const{
-	string buffer;
-	switch (this->type){
-	case Type::SINGLE:
-		buffer = "Single Card";
-		break;
-	case Type::PAIR:
-		buffer = "Pair";
-		break;
-	case Type::TRIO:
-		buffer = "Triple";
-		break;
-	case Type::TRIO_WITH_ONE:
-		buffer = "Triple With One";
-		break;
-	case Type::FOUR_WITH_TWO:
-		buffer = "Four With Two";
-		break;
-	case Type::SINGLE_CONTINUOUS:
-		buffer = "Single Continuous";
-		break;
-	case Type::PAIR_CONTINUOUS:
-		buffer = "Pair Continuous";
-		break;
-	case Type::TRIO_CONTINUOUS:
-		buffer = "Triple Continuous";
-		break;
-	case Type::PLANE:
-		buffer = "Plane With Wings";
-		break;
-	case Type::BOMB:
-		buffer = "Bomb";
-		break;
-	case Type::ROCKET:
-		buffer = "Rocket";
-		break;
-	case Type::EMPTY:
-		buffer = "Empty";
-		break;
-	default:
-		buffer = "Empty";
-	}
+	string temp{};
+	if (this->type==Type::SINGLE){ temp+="Single Card";}
+	else if(this->type==Type::PAIR){temp+="Pair";}
+	else if(this->type==Type::TRIO){temp+="Triple";}
+	else if(this->type==Type::TRIO_WITH_ONE){temp+="Triple With One";}
+	else if(this->type==Type::FOUR_WITH_TWO){temp+="Four With Two";}
+	else if(this->type==Type::SINGLE_CONTINUOUS){temp+="Single Continuous";}
+	else if(this->type==Type::PAIR_CONTINUOUS){temp+="Pair Continuous";}
+	else if(this->type==Type::TRIO_CONTINUOUS){temp+="Triple Continuous";}
+	else if(this->type==Type::PLANE){temp+="Plane With Wings";}
+	else if(this->type==Type::BOMB){temp+="Bomb";}
+	else if(this->type==Type::ROCKET){temp+="Rocket";}
+	else if(this->type==Type::EMPTY){temp+="Empty";}
+	else{temp+="Empty";}
+	return temp;
 }//return the type by string
 
-//accessors
 CardsType::Type CardsType::get_type() const{
 	return this->type;
 }
@@ -69,7 +52,7 @@ bool CardsType::is_continuous() const{
 	return this->continuous;
 }
 
-//mutators
+//Mutators
 void CardsType::set_type(Type type){
 	this->type = type;
 }
@@ -82,4 +65,19 @@ void CardsType::set_num_same_value(int num_same_value){
 void CardsType::set_continuous(bool continuous){
 	this->continuous = continuous;
 }
+
+//Static Functions
+bool CardsType::is_equal(const CardsType& a,const CardsType& b){
+	if(a.operator==(b)){return true;}
+	return false;
+}//return true if two types are exactly the same
+
+bool CardsType::is_comparable(const CardsType& a,const CardsType& b){
+	if(is_equal(a,b)){return true;}
+	else if(a.type==Type::BOMB || a.type==Type::ROCKET || b.type==Type::BOMB || b.type==Type::ROCKET){
+		return true;
+	}
+	return false;
+}//return true if two types are the same or ROCKET/BOMB exists
+
 
