e5594ff47d1d5c6689f8a35a620377802d241ccc
williamlus <67042315+williamlus@users.noreply.github.com>
2020-11-09 22:29:07 +0800

    Update CardsType.h
    1. Add some static functions to CardsType
    2. Change the former is_equal() to operator==()
    3. Use is_equal(a,b) to compare two CardsType objects

--- USTlord/CardsType.h
+++ USTlord/CardsType.h
@@ -5,10 +5,12 @@ using std::string;
 
 class CardsType {
 public:
-    enum class Type {
-        EMPTY, ROCKET, BOMB, SINGLE, PAIR, TRIO, TRIO_WITH_ONE,
-        SINGLE_CONTINUOUS, PAIR_CONTINUOUS, TRIO_CONTINUOUS,
-        PLANE, FOUR_WITH_TWO
+    enum Type {
+        SINGLE, PAIR, TRIO,
+        TRIO_WITH_ONE, FOUR_WITH_TWO,
+        SINGLE_CONTINUOUS, PAIR_CONTINUOUS, TRIO_CONTINUOUS, PLANE,
+        BOMB, ROCKET, 
+        EMPTY
     };
 private:
     Type type;
@@ -16,20 +18,29 @@ private:
     int num_same_value;//number of cards with same value
     bool continuous;//whether the cards are continuous
 public:
+    //Constructors and Destructor
     CardsType();
+    CardsType(Type type,int num_cards,int num_same_value,bool continuous);
     ~CardsType();
-    bool is_equal(const CardsType& ct) const;//compare whether two cardstype object are equal(can also be defined as bool operator==(const CardsTpe& ct);)
-    string to_string() const;//return the type by string
 
-    //accessors
+    //Binary Operation
+    bool operator==(const CardsType& ct) const;//compare whether two cardstype object are equal(can also be defined as bool operator==(const CardsTpe& ct);)
+    
+
+    //Accessors
+    string to_string() const;//return the type by string
     Type get_type() const;
     int get_num_cards() const;
     int get_num_same_value() const;
     bool is_continuous() const;
 
-    //mutators
+    //Mutators
     void set_type(Type type);
     void set_num_cards(int num_cards);
     void set_num_same_value(int num_same_value);
     void set_continuous(bool continuous);
+
+    //Static Functions
+    static bool is_equal(const CardsType& a,const CardsType& b);//return true if two types are exactly the same
+    static bool is_comparable(const CardsType& a,const CardsType& b);//return true if two types are the same or ROCKET/BOMB exists
 };
