7063001269131ac07dced01f637d24166c3d01b5
williamlus <67042315+williamlus@users.noreply.github.com>
2020-11-10 12:48:35 +0800

    Update CardsGroup.cpp

--- USTlord/CardsGroup.cpp
+++ USTlord/CardsGroup.cpp
@@ -20,6 +20,10 @@ void CardsGroup::reset(vector<const Card*> cards){
 }//reset CardsGroup
 
 //helper function
+int CardsGroup::count_max_continuous_times(vector<int> figures_distribution,int repeat_times){
+	//TBC
+}
+
 bool CardsGroup::check_continuous(vector<int> figures_distribution,int repeat_times){
 	//to ensure the length of figures_distribution == NUMBER_OF_FIGURES
 	if(figures_distribution.size()!=NUMBER_OF_FIGURES){
@@ -116,12 +120,59 @@ void CardsGroup::arrange(){
 	}
 	/*Case II : max_repeat==2*/
 	if(max_repeat==2){
-		//TBC
+		if(fig_dis_sorted[1]==0){
+			//consider the case of PAIR
+			if(figures_distribution[NUMBER_OF_FIGURES-1]==0){
+				this->cards_type.reset(CardsType::Type::PAIR,2,2,false);
+			}
+			//consider the case of ROCKET
+			else{
+				this->cards_type.reset(CardsType::Type::ROCKET,2,2,false);
+			}
+		}
+		else{
+			//consider the case of PAIR_CONTINUOUS
+			bool is_cont=check_continuous(figures_distribution,2);
+			if(is_cont){
+				this->cards_type.reset(CardsType::Type::PAIR_CONTINUOUS,cards.size(),2,true);
+			}
+			else{
+				this->cards_type.reset(CardsType::Type::EMPTY,cards.size(),2,false);
+			}
+		}
 	}
 
 	/*Case III : max_repeat==3*/
 	if(max_repeat==3){
-		//TBC
+		if(fig_dis_sorted[1]==0){
+			//consider the case of TRIO
+			if(figures_distribution[NUMBER_OF_FIGURES-1]==0){
+				this->cards_type.reset(CardsType::Type::TRIO,3,3,false);
+			}
+			else{
+				this->cards_type.reset(CardsType::Type::EMPTY,3,3,false);
+			}
+		}
+		//consider the case of TRIO_WITH_ONE
+		else if(fig_dis_sorted[1]==1 && fig_dis_sorted[2]==0){
+			this->cards_type.reset(CardsType::Type::TRIO_WITH_ONE,4,3,false);
+		}
+		else{
+			//consider the case of TRIO_CONTINUOUS
+			bool is_cont=check_continuous(figures_distribution,3);
+			if(is_cont){
+				this->cards_type.reset(CardsType::Type::PAIR_CONTINUOUS,cards.size(),3,true);
+			}
+			
+			//consider PLANE!!!!!
+			//Add helper func int count_max_cont_times(fig_dis,rep_times);
+			//need to modify check_continuous (for PAIR_CONT,TRIO_CONT, it suffices to find cards.size>=5)
+			else if(!is_cont){}
+
+			else{
+				this->cards_type.reset(CardsType::Type::EMPTY,cards.size(),3,false);
+			}
+		}
 	}
 	/*Case IV : max_repeat==4*/
 	if(max_repeat==4){
