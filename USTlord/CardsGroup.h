99245ff039003db5184bc51bfd30cd462f8e2091
williamlus <67042315+williamlus@users.noreply.github.com>
2020-11-10 23:27:23 +0800

    Update CardsGroup.h

--- USTlord/CardsGroup.h
+++ USTlord/CardsGroup.h
@@ -17,9 +17,13 @@ public:
     CardsGroup(vector<const Card*> cards);
     ~CardsGroup();
     
+    int compare(const CardsGroup& a) const;
+    //-2 when not comparable, -1 when *this<a, 0 when *this==a, 1 when *this>a 
+
+    //Mutator
     void reset(vector<const Card*> cards);//reset CardsGroup
-    
-    //accessor
+
+    //Accessors
     const Card* operator[](int i) const;
     vector<const Card*> get_cards() const;
     CardsType get_cards_type() const;
@@ -27,7 +31,7 @@ public:
     bool is_valid() const;//check whether this is a legal group to play
     vector<int> get_figures_distribution() const;
 
-    //static functions
+    //Static Functions
     static int count_max_continuous_times(vector<int> figures_distribution,int repeat_times);
     static bool check_continuous(vector<int> figures_distribution,int repeat_times);//check whether CONTINUOUS exists
 };
