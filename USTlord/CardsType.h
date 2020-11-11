a688449e14ce3a6d591e75740ff980904c8b487d
williamlus <67042315+williamlus@users.noreply.github.com>
2020-11-11 11:58:29 +0800

    Update CardsType.h

--- USTlord/CardsType.h
+++ USTlord/CardsType.h
@@ -21,15 +21,6 @@ private:
     int num_cards;//number of cards in this group
     int max_repeat_times;//number of cards with same value
     bool continuous;//whether the cards are continuous
-
-protected:
-    //Mutators
-    void reset(Type type,int num_cards,int max_repeat_times,bool continuous);
-    void set_type(Type type);
-    void set_num_cards(int num_cards);
-    void set_max_repeat_times(int max_repeat_times);
-    void set_continuous(bool continuous);
-
 public:
     //Constructors and Destructor
     CardsType();
@@ -39,12 +30,20 @@ public:
     //Binary Operation
     bool operator==(const CardsType& ct) const;//compare whether two cardstype object are equal(can also be defined as bool operator==(const CardsTpe& ct);)
     
+
     //Accessors
     string to_string() const;//return the type by string
     Type get_type() const;
     int get_num_cards() const;
     int get_max_repeat_times() const;
     bool is_continuous() const;
+
+    //Mutators
+    void reset(Type type,int num_cards,int max_repeat_times,bool continuous);
+    void set_type(Type type);
+    void set_num_cards(int num_cards);
+    void set_max_repeat_times(int max_repeat_times);
+    void set_continuous(bool continuous);
 
     //Static Functions
     static bool is_equal(const CardsType& a,const CardsType& b);//return true if two types are exactly the same
