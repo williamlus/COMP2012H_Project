cd0ee20f6fe1302241f2a42b92f9cc05c4bb0114
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2020-11-11 12:00:21 +0800

    Update Deck.h

--- USTlord/Deck.h
+++ USTlord/Deck.h
@@ -10,7 +10,7 @@ class Deck {
       vector<Card*> cards;//cards in Deck
       int number_of_cards;//Number of cards in deck
     public:
-      Deck();//default constructor
+      Deck();//default constructor, for 54 cards case
       Deck(vector<Card*> card);//Conersion constructor, i means the number of cards in this deck
       ~Deck();
       Card* operator[](int i);
