6056eefe558159f80c30b0334d0b45dfe54ba273
williamlus <67042315+williamlus@users.noreply.github.com>
2020-11-12 22:30:08 +0800

    Update Board.h

--- USTlord/Board.h
+++ USTlord/Board.h
@@ -8,7 +8,7 @@
 #include "CardsType.h"
 #include "CardsGroup.h"
 #include "Deck.h"
-#include "CurrenrPattern.h"
+#include "CurrentPattern.h"
 #include "Player.h"
 #define NUMBER_OF_PLAYER 3
 #define NUMBER_OF_CARDS 54
