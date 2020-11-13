9e25816ceabd175f531fed01ae75b41b2e2abf6d
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2020-11-13 18:47:27 +0800

    Update Deck.cpp
    add two overload version of get_certain_card

--- USTlord/Deck.cpp
+++ USTlord/Deck.cpp
@@ -42,10 +42,19 @@ vector<Card const*> Deck::get_cards() const{
     return this->cards;
 }
 
-Card* Deck::get_certain_card(Card::Color color, int value) {
+const Card* Deck::get_certain_card(Card::Color color, int value) {
     Card* temp_card = new Card(color, value);
     for (int i = 0; i < cards.size(); ++i) {
-        if(Card::strictly_equal(temp_card,cards[i]){return cards[i]; }
+        if(Card::strictly_equal(temp_card,cards[i])){return cards[i]; }
+    }
+    delete temp_card;
+        return nullptr;
+}
+
+const Card* Deck::get_certain_card(int value){
+    Card* temp_card = new Card(Card::Color::SPADE, value);
+    for (int i = 0; i < cards.size(); ++i) {
+        if(*temp_card==*cards[i]){return cards[i]; }
     }
     delete temp_card;
         return nullptr;
