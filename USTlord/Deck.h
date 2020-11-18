5973865a576ff23a373578f281a36c323bee3e7a
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2020-11-18 12:02:52 +0800

    Update Deck.h

--- USTlord/Deck.h
+++ USTlord/Deck.h
@@ -5,13 +5,19 @@
 #include <vector>
 #include <string>
 #include "Card.h"
+#include "CardsGroup.h"
 
 class Deck {
     private:
       vector<Card const*> cards;//cards in Deck
-      vector<CardsGroup> combination;
+      vector<Card const*> cards_in_combination;
+      vector<Card const*> cards_in_important_combination;
       void generate_combination();
       bool exist(Card const*);
+      bool exist(Card const*,vector<Card const*>);
+      //helper function for generate_combination
+      const Card* get_certain_card_base(int value,vector<Card const*> chosen);
+      bool split_important_combination {false};//a signal of whether this hint must split the important combination. This is used for AI player
     public:
       Deck();//default constructor, generate an empty deck
       Deck(vector<Card const*> card);//Conversion constructor
@@ -24,8 +30,9 @@ class Deck {
       int get_num_cards() const;
       vector<Card const*> get_cards() const;
       const Card* get_certain_card(Card::Color color, int value);
-      const Card* get_certain_card(int value,Card const* check);
+      const Card* get_certain_card(int value,vector<Card const*> chosen );
       const Card* get_certain_card(vector<Card const*> chosen);
+      //for all get_certain_card function, before invoke it, make sure such certain card exist, otherwise it will return nullptr
       static vector<int> get_deck_distribution(Deck* deck);
 };
 
