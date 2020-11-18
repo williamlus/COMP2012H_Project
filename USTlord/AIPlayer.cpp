19fe408d33e61aa0400f837147b956964df1e0b4
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2020-11-18 12:13:34 +0800

    Update AIPlayer.cpp

--- USTlord/AIPlayer.cpp
+++ USTlord/AIPlayer.cpp
@@ -12,11 +12,16 @@ const CardsGroup& AIPlayer::choose_hint(const CurrentPattern& cp, vector<int> pl
 }//choose cardsgroup according to the situation of board (e.g. num of cards of other players, current_pattern)
 
 CardsGroup AIPlayer::play(const CurrentPattern& cp, vector<int> players_num_cards){
+    if(!deck->split_important_combination){
     this->calc_hints(cp);
-    return this->choose_hint(cp, players_num_cards);
+    return this->choose_hint(cp, players_num_cards);}
+    else{
+        return CardsGroup();
+    }
 }//use cin or hint (with loops) to Play cards according to current pattern, clear_cards, and reset data members
 
 bool AIPlayer::want_landlord(){
+    
     srand(time(0));
     return rand()%2;//temporary settings (please increase AI's intelligence)
 }//choose to be landlord or not
