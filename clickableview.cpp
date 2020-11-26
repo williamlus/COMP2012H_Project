ae3f2e47e6c5abee423c78a039e04b6ed1ac3c73
ZhengHantao <61699822+ZhengHantao@users.noreply.github.com>
2020-11-27 01:13:50 +0800

    Delete clickableview.cpp

--- clickableview.cpp
+++
@@ -1,9 +0,0 @@
-#include <QMouseEvent>
-
-#include "clickableview.h"
-
-ClickableView::ClickableView(QWidget *parent):QGraphicsView(parent) {}
-
-void ClickableView::mousePressEvent(QMouseEvent *event) {
-
-}
