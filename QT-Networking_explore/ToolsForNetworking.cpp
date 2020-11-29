c0fbabde391303cf31dd8fa94a007fb684f2e663
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2020-11-29 20:27:33 +0800

    Update ToolsForNetworking.cpp

--- QT-Networking_explore/ToolsForNetworking.cpp
+++ QT-Networking_explore/ToolsForNetworking.cpp
@@ -1,3 +1,6 @@
+#include "ToolsForNetworking.h"
+#include <QtNetwork>
+
 class Player_info{
 	public:
 
@@ -5,7 +8,7 @@ class Player_info{
 	unsigned int cards_remain;
 	int role;//-1 for null, 0 for landlord, 1 for farmer
 	
-	Player_info(player_index=-1.cards_remain=-1,role=-1) : player_index(player_index), cards_remain(cards_remain), role(role) {}
+	Player_info(int player_index=-1, int cards_remain=-1, int role=-1) : player_index(player_index), cards_remain(cards_remain), role(role) {}
 	
 };
 
@@ -23,7 +26,7 @@ class DataPackage{
 	QVector <Player_Info> player_info;
 	int active_player_id;
 
-	DataPackage(data_type=-1, sender_id=-1, cards={}, player_info={}, active_player_id=-1) :
+	DataPackage(int data_type=-1, int sender_id=-1, QVector <Card> cards={}, QVector <Player_Info> player_info={}, int active_player_id=-1) :
 	data_tpe(data_type), sender_id(sender_id), cards(cards), player_info(player_info), active_player_id(active_player_id) {}
 
 	friend QDataStream& operator>>(QDataStream& in, DataPackage& data){
