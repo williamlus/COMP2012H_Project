f4e62d0428401f55616b935d8548db019a6d25e1
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2020-11-30 20:11:57 +0800

    Update server.h

--- USTlord_final/source/server.h
+++ USTlord_final/source/server.h
@@ -13,6 +13,13 @@ class Server : public QTcpServer
 public:
     explicit Server(QObject *parent = nullptr);
 private:
+    QTcpServer* serverSocket;
+    MyTools tool;
+    QVector<Card> cards{};
+    QVector<Player_Info> player_info{};
+    QVector<QString> message{};
+    int active_player_id{-1};
+
     QVector<Client*> clients{};
     DataPackage* data_to_send{nullptr};
     QVector<DataPackage*> data_received{};
