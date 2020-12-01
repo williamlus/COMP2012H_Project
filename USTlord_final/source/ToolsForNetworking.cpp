f255cb712df8ee368a965e0fb798b3be469e042e
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2020-12-01 19:37:51 +0800

    Update ToolsForNetworking.cpp

--- USTlord_final/source/ToolsForNetworking.cpp
+++ USTlord_final/source/ToolsForNetworking.cpp
@@ -3,6 +3,41 @@
 
 MyTools::MyTools(){}
 
+void MyTools::read(QTcpSocket* socket)
+{
+    DataPackage data;
+    QDataStream in(socket);
+    in.setVersion(QDataStream::Qt_5_12);
+    in.setByteOrder(QDataStream::BigEndian);
+    static qint32 bytesToRead = 0;
+    //qDebug() << "socket->bytesAvailable(): " << socket->bytesAvailable();
+    if (static_cast<qint32>(socket->bytesAvailable()) < static_cast<qint32>(sizeof(qint32)))
+        return; // the head info of size is incomplete
+    while (socket->bytesAvailable())
+    {
+        if (static_cast<qint32>(socket->bytesAvailable()) >= static_cast<qint32>(sizeof(qint32)) && bytesToRead == 0)
+            in >> bytesToRead;  // read the size of following data
+        //qDebug() << "socket->bytesAvailable(): " << socket->bytesAvailable();
+        if (socket->bytesAvailable() >= bytesToRead)
+        {
+            // read data
+            //qDebug() << "start reading data...";
+            char* temp = new char[static_cast<unsigned long>(bytesToRead + 1)]{ 0 };
+            in.readRawData(temp, bytesToRead);
+            //qDebug() << "socket->bytesAvailable(): " << socket->bytesAvailable();
+            QByteArray buffer(temp, bytesToRead);
+            QDataStream stream(&buffer, QIODevice::ReadWrite);
+            stream >> data;
+            bytesToRead = 0;
+            emit transferPackage(data);
+        }
+       else
+            break;  // the data is incomplete; read it next time
+    }
+}
+
+
+
 
 
 
