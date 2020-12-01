e3b86196546cce1ca0fe43eb7bde40942bbe7e5b
williamlus <67042315+williamlus@users.noreply.github.com>
2020-12-01 11:27:05 +0800

    Add files via upload

--- USTlord_final/clientwindow.cpp
+++ USTlord_final/clientwindow.cpp
@@ -7,6 +7,7 @@ ClientWindow::ClientWindow(QWidget *parent) :
 {
     ui->setupUi(this);
     this->setFixedSize(800,600);
+
 }
 
 void ClientWindow::closeEvent(QCloseEvent *event)
@@ -22,6 +23,11 @@ ClientWindow::~ClientWindow()
     delete ui;
 }
 
+void ClientWindow::setPort(const int port)
+{
+    ui->label_port->setText(QString(port));
+}
+
 void ClientWindow::on_pushButton_quit_clicked()
 {
     this->close();
@@ -29,10 +35,56 @@ void ClientWindow::on_pushButton_quit_clicked()
 
 void ClientWindow::on_pushButton_join_server_clicked()
 {
-
+    if(client){
+        client->close();
+    }
+    client = new QTcpSocket();
+    // connected with the server
+    connect(client, &QTcpSocket::connected, [=]{ qDebug() << "connected"; });
+    connect(client,SIGNAL(error(QAbstractSocket::SocketError)),this,SLOT(displayError(QAbstractSocket::SocketError)));
+    // "new data is available for reading from the device's current read channel"
+    connect(client, &QTcpSocket::readyRead, this, &ClientWindow::readyRead);
+    connect(&my_tool, &MyTools::transferPackage, this, &ClientWindow::received_from_server);
+    client->connectToHost(ui->label_serverIP->text(), static_cast<quint16>(ui->label_port->text().toInt()));
 }
 
 void ClientWindow::on_pushButton_stop_joining_clicked()
 {
+    disconnect(client,SIGNAL(error(QAbstractSocket::SocketError)),this,SLOT(displayError(QAbstractSocket::SocketError)));
+    // "new data is available for reading from the device's current read channel"
+    disconnect(client, &QTcpSocket::readyRead, this, &ClientWindow::readyRead);
+    client->close();
+    client=nullptr;
+}
 
+void ClientWindow::readyRead()
+{
+    my_tool.read(client);
+}
+
+void ClientWindow::displayError(QAbstractSocket::SocketError)
+{
+    qDebug() << "error" << client->errorString();
+    on_pushButton_stop_joining_clicked();
+}//display error and close the client
+
+void ClientWindow::received_from_server(DataPackage data)
+{
+    if(data.data_type==-1){return;}
+    if(data.data_type==0){
+        play_window = new PlayWindow(data,this);
+        connect(play_window,&PlayWindow::send_datapackage,this,&ClientWindow::received_from_playwindow);
+        this->hide();
+        play_window->show();
+    }
+    else{
+        qDebug() << "Received id: " + QString::number(data.active_player_id);
+        play_window->receive_datapackage(data);
+    }
+}
+
+void ClientWindow::received_from_playwindow(DataPackage data){
+    if(client!=nullptr){
+        my_tool.send(client,data);
+    }
 }
--- USTlord_final/clientwindow.h
+++ USTlord_final/clientwindow.h
@@ -4,6 +4,8 @@
 #include <QMainWindow>
 #include <QCloseEvent>
 #include "source/client.h"
+#include "source/ToolsForNetworking.h"
+#include "playwindow.h"
 
 namespace Ui {
 class ClientWindow;
@@ -15,19 +17,37 @@ class ClientWindow : public QMainWindow
 
 public:
     explicit ClientWindow(QWidget *parent = nullptr);
+
     void closeEvent(QCloseEvent *event) override;
+
     ~ClientWindow();
 
-private slots:
+    void setPort(const int port);//modify the port by line edit
+
+public slots:
     void on_pushButton_quit_clicked();
 
     void on_pushButton_join_server_clicked();
 
     void on_pushButton_stop_joining_clicked();
 
+    void readyRead();//let my_tool to process the raw data
+
+    void displayError(QAbstractSocket::SocketError);//display error, close the client and set to nullptr
+
+    void received_from_server(DataPackage data);
+    //when ready_read signal occurs, trigger received_from_server
+    //if the data is "start game", create playwindow by conversion constructor ( i.e. PlayWindow(DataPackage,this) )
+    //else, send the data to playwindow by play_window->receive_package(DataPackage)
+    void received_from_playwindow(DataPackage data);
+    //when playwindow emit signal send_datapackage(Datapackage), trigger received_from_playwindow
+    //send the data to server by my_tool
+
 private:
     Ui::ClientWindow *ui;
-    Client *client{nullptr};
+    QTcpSocket *client{nullptr};
+    MyTools my_tool;
+    PlayWindow* play_window{nullptr};
 };
 
 #endif // CLIENTWINDOW_H
