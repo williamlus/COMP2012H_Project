16a8904f0d42c13eb60c5a3a640fd70b80ec558e
SU-Hongs <74102512+SU-Hongs@users.noreply.github.com>
2020-12-03 23:09:06 +0800

    Update AIPlayer.h

--- USTlord_final/source/AIPlayer.h
+++ USTlord_final/source/AIPlayer.h
@@ -17,9 +17,36 @@ class AIPlayer : public Player {
       AIPlayer();
       AIPlayer(int id,string name);
       ~AIPlayer();
-      const CardsGroup choose_hint(const CurrentPattern& cp);//choose cardsgroup according to the situation of board (e.g. num of cards of other players, current_pattern)
-      CardsGroup play(const CurrentPattern& cp, vector<int> players_num_cards) override;//use calc_hints, get_hint, choose_hint to play cards according to current pattern, clear_cards to be played, and reset data members
+      const CardsGroup choose_hint(const CurrentPattern& cp);
+      /*
+       * AI player with some common sense strategy:
+
+       * if the AI player is the first one to play or the CurrentPattern is played by himself
+         he will play cards with the following logic:
+
+         First find the smallest card in hand,
+         if it is inside a bomb, then DO NOT play it, and find the second smallest one unless he only has one card left
+         if not, then if this card is inside any other cards_combination, then just play this combination
+         otherwise just play this SINGLE card
+
+       * if the AI player is landlord, then try to beat everyone
+
+       * if the AI player is farmer, then beat landlord all the time
+         if the CurrentPattern is played by another farmer
+         Only give cards in these cases:
+         * the AI player can directly win the game
+         * the partner played a small SINGLE or PAIR
+
+       */
+
+      CardsGroup play(const CurrentPattern& cp) override;//use calc_hints, get_hint, choose_hint to play cards according to current pattern, clear_cards to be played, and reset data members
       bool want_landlord() override;//choose to be landlord or not automatically
+      /*
+       * the probability of choosing lord is determined by how good the cards in hand are
+         The evaluation is basically determined by the number of cards_combination
+         The cards_combination considered here are:
+         TRIO,BOMB, ROCKET
+       */
 };
 
 #endif /* AIPLAYER_H_ */
