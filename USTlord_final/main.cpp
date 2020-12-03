05ee0cc98ff15e476a10d20d1046a364133a590b
williamlus <67042315+williamlus@users.noreply.github.com>
2020-12-03 17:31:57 +0800

    Update main.cpp

--- USTlord_final/main.cpp
+++ USTlord_final/main.cpp
@@ -2,6 +2,16 @@
 
 #include <QApplication>
 
+const QString DataPackage::Content::REQUEST = "request";
+const QString DataPackage::Content::ACCEPT="accept";
+const QString DataPackage::Content::REJECT="reject";
+const QString DataPackage::Content::BE_LANDLORD="be";
+const QString DataPackage::Content::DO_NOT_PLAY="noplay";
+const QString DataPackage::Content::WIN_GAME="win";
+const QString DataPackage::Content::LOSE_GAME="lose";
+const QString DataPackage::Content::END_GAME="end";
+const QString DataPackage::Content::QUIT="quit";
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
