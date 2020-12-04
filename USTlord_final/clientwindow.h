97eeedc5f341c65679fd2389e1c3a11413b3373b
williamlus <67042315+williamlus@users.noreply.github.com>
2020-12-04 19:17:10 +0800

    Update clientwindow.h

--- USTlord_final/clientwindow.h
+++ USTlord_final/clientwindow.h
@@ -1,64 +1,43 @@
-#ifndef SERVERWINDOW_H
-#define SERVERWINDOW_H
+#ifndef CLIENTWINDOW_H
+#define CLIENTWINDOW_H
 
 #include <QMainWindow>
-#include <QCloseEvent>
-#include <QTcpServer>
 #include <QTcpSocket>
-#include <QHostInfo>
-#include <QVector>
-#include "clientwindow.h"
-#include "source/Card.h"
+#include <QCloseEvent>
 #include "source/datapackage.h"
+#include "playwindow.h"
 
 namespace Ui {
-class ServerWindow;
+class ClientWindow;
 }
 
-class ServerWindow : public QMainWindow
+class ClientWindow : public QMainWindow
 {
     Q_OBJECT
 
 public:
-    explicit ServerWindow(QWidget *parent = nullptr);
+    explicit ClientWindow(QWidget *parent = nullptr);
     void closeEvent(QCloseEvent *event) override;
-    QVector<QString> get_local_IP() const;
-    ~ServerWindow();
+    ~ClientWindow();
 
-    signals:
-    //void receive_data(DataPackage data);
+public slots:
+    void handleStateChanged(QAbstractSocket::SocketState state);
+    void handle_server_message();
+    void send_to_server(DataPackage data);
+    void received_from_playwindow(DataPackage data);
 
 private slots:
-    void on_pushButton_stop_clicked();
+    void on_pushButton_join_server_clicked();
 
-    void on_pushButton_create_clicked();
+    void on_pushButton_stop_joining_clicked();
 
-    void on_pushButton_start_game_clicked();
-
-    void handleConnection();
-
-    void handleStateChanged(QAbstractSocket::SocketState state);
-    void handle_clients_message();
-    void sendData(QTcpSocket* socket,DataPackage data);
-    void receiveData(DataPackage data);
-
-    void give_id();
-    void confirm_ready();
-    void deal_cards();
-    void choose_landlord(DataPackage data);
-    void init_game();
+    void on_pushButton_quit_clicked();
 
 private:
-    Ui::ServerWindow *ui;
-
-    QTcpServer* server{nullptr};
-    QVector<QTcpSocket*> clients{};
-    ClientWindow* client_window{nullptr};
-    DataPackage* data_to_send{nullptr};
-    QVector<DataPackage> data_received{};
-    QString cards;
-    QString bonus_cards;
-    QVector<QString> names={"","",""};
+    Ui::ClientWindow *ui;
+    QTcpSocket *socket{nullptr};
+    PlayWindow *play_window{nullptr};
+    int id{-1};
 };
 
-#endif // SERVERWINDOW_H
+#endif // CLIENTWINDOW_H
